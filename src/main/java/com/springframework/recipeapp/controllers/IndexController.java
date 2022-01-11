1a24c8130c151f4084adf9db8bad585ea3579375
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-11 11:52:38 +0000

    Configuring IntelliJ to allow code change during runtime

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+        System.out.println("Some message to say... beep boop bop");
         return "index";
     }
 }
