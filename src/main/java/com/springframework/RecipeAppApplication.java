ef142f5766cae96a493187ae5a51a220b3ab38f4
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-12 10:16:37 +0000

    Added notes, recipe, and ingredient entities. Set JPA relationships for notes and recipe

--- src/main/java/com/springframework/RecipeAppApplication.java
+++ src/main/java/com/springframework/RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package com.springframework.recipeapp;
+package com.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/springframework/domain/Ingredient.java
@@ -0,0 +1,9 @@
+package com.springframework.domain;
+
+import java.math.BigDecimal;
+
+public class Ingredient {
+
+    private String description;
+    private BigDecimal amount;
+}
