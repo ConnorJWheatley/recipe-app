710c6393224dcb7395c2aee685b00896f17c1d67
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-13 12:01:21 +0000

    Starting to add JUnit tests, also using mockito

---
+++ src/test/java/com/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,12 @@
+package com.springframework.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    @Test
+    public void getIndexPage() {
+    }
+}
