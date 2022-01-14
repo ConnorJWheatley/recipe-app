703a37ff34056bd4067360c779018af4baa5d2c1
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-14 16:44:26 +0000

    Added recipeform to enter new data into database, updated recipe controller

--- src/main/java/com/springframework/controllers/RecipeController.java
+++ src/main/java/com/springframework/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package com.springframework.controllers;
 
+import com.springframework.commands.RecipeCommand;
 import com.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -21,5 +21,18 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
