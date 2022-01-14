0ad1b9e2ecacc175ee0f36a6bf3814881121c156
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-14 13:30:23 +0000

    Display the rest of recipe properties

--- src/test/java/com/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/controllers/RecipeControllerTest.java
@@ -15,8 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class RecipeControllerTest {
 
@@ -47,6 +46,7 @@ class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 }
