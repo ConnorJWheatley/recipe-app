2769ca3ff617fa790c33fb629075b5f9eda0d1d1
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-17 17:32:54 +0000

    Implemented adding images to database

--- src/main/java/com/springframework/services/ImageServiceImpl.java
+++ src/main/java/com/springframework/services/ImageServiceImpl.java
@@ -1,16 +1,43 @@
 package com.springframework.services;
 
+import com.springframework.domain.Recipe;
+import com.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
 
-        log.debug("Received a file");
+            Byte[] byteObjects = new Byte[file.getBytes().length];
 
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
     }
 }
