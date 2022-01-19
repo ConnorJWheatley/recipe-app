eefa3f1c55055b5825befa51fac7cf8ae4a7bd22
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-19 11:55:11 +0000

    Added number format exception handling

--- src/test/java/com/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/controllers/RecipeControllerTest.java
@@ -61,6 +61,15 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
